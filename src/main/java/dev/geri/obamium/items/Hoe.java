9188029f8d73d1ba168c02d922ec4559fbf5ae2c
NotGeri <admin@geri.dev>
2022-03-18 21:32:07 +0100

    Add tools, armour, basic world gen for oures

---
+++ src/main/java/dev/geri/obamium/items/Hoe.java
@@ -0,0 +1,12 @@
+package dev.geri.obamium.items;
+
+import net.minecraft.item.HoeItem;
+import net.minecraft.item.ToolMaterial;
+
+public class Hoe extends HoeItem {
+
+    public Hoe(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
+    }
+    
+}
